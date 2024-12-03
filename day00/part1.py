598fe9fd859716eda6d3170a94d66be27f5992fe
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2024-12-03 14:19:28 -0500

    reorganized template file

--- day00/part1.py
+++ day00/part1.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 import argparse
-import os.path
-
 import pytest
+import os.path
 
 import support
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
-
 
 def compute(s: str) -> int:
     numbers = support.parse_numbers_split(s)
@@ -20,21 +18,6 @@ def compute(s: str) -> int:
         pass
     # TODO: implement solution here!
     return 0
-
-
-INPUT_S = """\
-
-"""
-EXPECTED = 1
-
-
-@pytest.mark.parametrize(
-    ("input_s", "expected"),
-    ((INPUT_S, EXPECTED),),
-)
-def test(input_s: str, expected: int) -> None:
-    assert compute(input_s) == expected
-
 
 def main() -> int:
     parser = argparse.ArgumentParser()
@@ -49,3 +32,22 @@ def main() -> int:
 
 if __name__ == "__main__":
     raise SystemExit(main())
+
+
+###############
+#   TESTING   #
+###############
+
+INPUT_S = """\
+
+"""
+EXPECTED = 1
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
