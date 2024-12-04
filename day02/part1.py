b0d5df13b168fdc5abcdf206ea8e0565f70a4c30
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2024-12-04 18:02:43 -0500

    Revert "new testing format"
    This reverts commit f69ef7b7ddd294076f005e14135f071a9f317497.

--- day02/part1.py
+++ day02/part1.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import argparse
 import os.path
+import pytest
 import support
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
 def compute(s: str) -> int:
@@ -20,8 +21,8 @@ def compute(s: str) -> int:
 MAX_DIFFERENCE = 3
 
 
-def is_safe(ar: list[int]) -> bool:
-    differs = [a - b for a, b in zip(ar, ar[1:])]
+def is_safe(l: list[int]) -> bool:
+    differs = [a - b for a, b in zip(l, l[1:])]
 
     isMonotonic = all(i > 0 for i in differs) or all(i < 0 for i in differs)
     inRange = all(abs(i) <= MAX_DIFFERENCE for i in differs)
@@ -29,9 +30,28 @@ def is_safe(ar: list[int]) -> bool:
     return isMonotonic and inRange
 
 
+INPUT_S = """\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
     args = parser.parse_args()
 
     with open(args.data_file) as f, support.timing():
@@ -40,5 +60,5 @@ def main() -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
--- day03/part1.py
+++ day03/part1.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 import argparse
 import os.path
+import pytest
 import re
 
 import support
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
 
-pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
 
 
 def compute(s: str) -> int:
@@ -19,9 +20,23 @@ def compute(s: str) -> int:
     return total
 
 
+INPUT_S = """\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
+EXPECTED = 161
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
     args = parser.parse_args()
 
     with open(args.data_file) as f, support.timing():
@@ -30,5 +45,5 @@ def main() -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
