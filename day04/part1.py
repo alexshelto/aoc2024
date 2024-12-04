52123d8de5d1330dd9e59de83d29180ab1d1e2e5
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2024-12-04 12:04:18 -0500

    day04

---
+++ day04/part1.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import argparse
+import pytest
+import os.path
+
+from typing import Tuple
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+TARGET = "MAS"
+
+
+def compute(s: str) -> int:
+    count = 0
+    coords = support.parse_coords_str(s)
+
+    x_locs = [key for key, value in coords.items() if value == "X"]
+
+    for point in x_locs:
+        count += is_xmas(point, coords)
+    return count
+
+
+def is_xmas(p: tuple[int, int], coords: dict[tuple[int, int], str]) -> bool:
+    count = 0
+
+    # Take the next 3 points in each direction:
+    for offset in support.XY_8Offsets:
+        next_3_pts = support.GridNav.next_n_points(offset, p, 3)
+
+        built_word = [coords.get(point, ".") for point in next_3_pts]
+
+        if built_word == ["M", "A", "S"]:
+            count += 1
+
+    return count
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+
+
+###############
+#   TESTING   #
+###############
+INPUT_S = """\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+"""
+EXPECTED = 18
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
---
+++ day04/part2.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+import argparse
+import pytest
+import os.path
+
+from typing import Tuple
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    count = 0
+    coords = support.parse_coords_str(s)
+
+    a_locs = [key for key, value in coords.items() if value == "A"]
+
+    for point in a_locs:
+        if can_spell_xmas_from_point(point, coords):
+            count += 1
+
+    return count
+
+
+DIRECTIONS = [
+    [support.XY_8Offsets.UP_LEFT, support.XY_8Offsets.DOWN_RIGHT],
+    [support.XY_8Offsets.UP_RIGHT, support.XY_8Offsets.DOWN_LEFT],
+]
+
+
+def can_spell_xmas_from_point(
+    p: tuple[int, int], coords: dict[tuple[int, int], int]
+) -> bool:
+    count = 0
+
+    # Use offsets and get diagonal points
+    for offset in DIRECTIONS:
+        p1 = support.GridNav.next_n_points(offset[0], p, 1)[0]  # index 0 only 1 point
+        p2 = support.GridNav.next_n_points(offset[1], p, 1)[0]
+
+        letters = {coords.get(p1, "."), coords.get(p2, ".")}
+
+        if letters != {"M", "S"}:
+            return False
+
+    return True
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+
+
+###############
+#   TESTING   #
+###############
+INPUT_S = """\
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+"""
+EXPECTED = 9
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
